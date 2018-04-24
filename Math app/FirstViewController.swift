7ccd980782eac290d52ece987523e19484870401
Aditi <someshwar.aditi@gmail.com>
2018-04-24 09:37:57 +0800

    Initial Commit

---
+++ Math app/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Math app
+//
+//  Created by Aditi Someshwar on 24/4/2018.
+//  Copyright © 2018 Aditi Someshwar. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
